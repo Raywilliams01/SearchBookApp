b5b0887169539fe2225b289e5d287f86e99de7c2
ray <mr.raywilliams01@gmail.com>
2019-05-24 08:27:33 -0400

    adding xml for listview

--- app/src/main/java/com/example/searchbookapp/view/viewmodal/BookItemViewModal.kt
+++ app/src/main/java/com/example/searchbookapp/view/viewmodal/BookItemViewModal.kt
@@ -1,8 +1,36 @@
 package com.example.searchbookapp.view.viewmodal
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.searchbookapp.view.modal.BookItemList
 
 // Create View Modal for List View
+// To have live Data
 class BookItemViewModal: ViewModel() {
 
+    val bookItemList = MutableLiveData<List<BookItemList>>()
+    val bookLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        getBookList()
+    }
+
+    private fun getBookList() {
+        val mockBook = listOf(
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger"),
+            BookItemList(auth = "Bod", authBookTitle = "Burger")
+        )
+
+        bookLoadError.value = false
+        loading.value = false
+
+        bookItemList.value = mockBook
+    }
 }
